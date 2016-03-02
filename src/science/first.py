6b416fe2cf839a649ce0742ce226dc55eff2d642
jaythaceo <jaythaceo@gmail.com>
2016-03-02 17:19:19 -0500

    [chore] Added file

--- src/science/first.py
+++ src/science/first.py
@@ -3,11 +3,4 @@ Author: Jason Brooks
 Email: jaythaceo@gmail.com
 Twitter: @jaythaceo
 """
-def squaring(n):
-    """Squaring numbers n * n"""
-    n = n * n
-    return n
-
-def searcher(first, last):
-    """docstring for fnamsearcher"""
-
+print "Jason"
